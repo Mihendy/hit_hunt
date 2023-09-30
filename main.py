620be54e66e77e87cb4982dc13b1b44aaf7effb9
mihendy <kryukovm1khael@yandex.ru>
2023-09-30 23:00:20 +0500

    Validators by pydantic, exception handler, class Filter

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, Depends
 from fastapi.responses import JSONResponse
 
 from datetime import datetime as dt
 from datetime import timedelta
 from contextlib import asynccontextmanager
 
+from pydantic import ValidationError
+
 from data_base.sql_init import initialize
 from data_base.connection import DB_CONNECTION
 from data_base.queries import save_new_visitors
 
-from source.classes import Visitor
-from source.validators import str_datetime_validator, limit_validator, utc_validator
+from source.classes import Visitor, Filter
 
 banned_ips = dict()
 strange_ips = dict()
@@ -31,6 +32,20 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.exception_handler(ValidationError)
+async def value_error_exception_handler(request: Request, exc: ValidationError):
+    """Обработчик ошибок возникших в процессе валидации"""
+    error = [{"type": err["type"], "loc": err["loc"][0],
+              "msg": err["msg"].replace("Value error, ", ""),
+              "input": err["input"]} for err in exc.errors()]
+    print(error)
+
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": error}
+    )
 
 
 @app.middleware("http")
@@ -86,30 +101,9 @@ async def root(request: Request):
 
 
 @app.get("/api/v1/get/")
-async def v1_get_visitors(start: str = None, end: str = None, like: str = None, limit: int = 10,
-                          utc: float = 0, unique: bool = False):
-    errors = {}
-
-    if not str_datetime_validator(start):
-        errors["start"] = "Bad datetime format. HINT: DD-MM-YYYY:hh:mm:ss OR DD-MM-YYYY"
-
-    if not str_datetime_validator(end):
-        errors["end"] = "Bad datetime format. HINT: DD-MM-YYYY:hh:mm:ss OR DD-MM-YYYY"
-
-    if not limit_validator(limit):
-        errors["limit"] = "Bad number. HINT: 1 ≤ limit ≤ 100 and integer"
-
-    if not utc_validator(utc):
-        errors["utc"] = "Bad UTC format. (EXAMPLE: utc=1 OR utc=-3.5)"
-
-    if new_visitors and not errors:
+async def v1_get_visitors(filters: Filter = Depends()):
+    if new_visitors:
         save_new_visitors(DB_CONNECTION, new_visitors)
         new_visitors.clear()
-
     data = {"message": None}
-
-    if not errors:
-        return JSONResponse(status_code=status.HTTP_200_OK, content=data)
-    else:
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                            content={"message": "Bad request", "errors": errors})
+    return JSONResponse(status_code=status.HTTP_200_OK, content=data)
--- source/classes.py
+++ source/classes.py
@@ -1,9 +1,57 @@
-from datetime import datetime as dt
-from pydantic import BaseModel
+from datetime import datetime as dt, datetime
+from typing import Any, Optional
+
+from fastapi import Query
+from pydantic import (BaseModel, Field, conint,
+                      field_validator, model_validator)
 
 
 class Visitor(BaseModel):
     ip: str
-    datetime: str = str(dt.utcnow())
+    datetime: str = str(dt.utcnow())  # optional
     platform: str = "NULL"  # optional
     agent: str = "NULL"  # optional
+
+
+class Filter(BaseModel):
+    start: Optional[str] = Field(Query(None, description="Represents the start date & time"))
+    end: Optional[str] = Field(Query(None, description="Represents the end date & time"))
+    like: Optional[str] = Field(Query(None, description="Represents a keyword or pattern to filter results"
+                                                        " based on a partial match. Example: \"apple\" to "
+                                                        "filter results containing the word \"apple\"."))
+    limit: conint(ge=0, le=100) = 10
+    utc: float = Field(default=0, ge=-12, le=12)
+    unique: bool = False
+
+    @classmethod
+    @field_validator("start", "end")
+    def validate_str_datetime(cls, value):
+        """Проверка даты, написанной строкой слитно в
+            формате ДД-ММ-ГГГГ:чч:мм:cc (exp: 16-05-2022:01:50:30)"""
+        if value is None:
+            return value
+        try:
+            datetime.strptime(value, "%d-%m-%Y:%H:%M:%S")
+        except (ValueError, TypeError):
+            try:
+                datetime.strptime(value, "%d-%m-%Y")
+            except (ValueError, TypeError):
+                raise ValueError("Input should be DD-MM-YYYY:hh:mm:ss or DD-MM-YYYY")
+        return value
+
+    @classmethod
+    @field_validator("like")
+    def validate_isalnum(cls, value):
+        if value is None:
+            return value
+        if value.isalnum():
+            return value
+        raise ValueError("Input should contain letters or numbers")
+
+    @classmethod
+    @field_validator("utc")
+    def utc_validator(cls, utc):
+        """Проверка валидности UTC"""
+        if utc in map(lambda x: x / 10, range(-120, 121, 5)):
+            return utc
+        raise ValueError("Input should be an integer or half number")

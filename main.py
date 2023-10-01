52c1367f9c8f8734ea1a7d7d7ff5328296d9a810
mihendy <kryukovm1khael@yandex.ru>
2023-10-01 17:31:32 +0500

    Validators & Handler & CI/CD All validators are ready All errors are handling by handlers python-package.yml

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, status, Depends
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 from datetime import datetime as dt
@@ -19,7 +20,8 @@ new_visitors = list()
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(application: FastAPI):
+    """Жизненный цикл приложения"""
     # do something before the application starts taking requests
     initialize(DB_CONNECTION)
 
@@ -35,12 +37,12 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.exception_handler(ValidationError)
+@app.exception_handler(RequestValidationError)
 async def value_error_exception_handler(request: Request, exc: ValidationError):
     """Обработчик ошибок возникших в процессе валидации"""
-    error = [{"type": err["type"], "loc": err["loc"][0],
+    error = [{"type": err["type"], "loc": str(*err["loc"]),
               "msg": err["msg"].replace("Value error, ", ""),
               "input": err["input"]} for err in exc.errors()]
-    print(error)
 
     return JSONResponse(
         status_code=status.HTTP_400_BAD_REQUEST,
@@ -50,39 +52,38 @@ async def value_error_exception_handler(request: Request, exc: ValidationError):
 
 @app.middleware("http")
 async def validate_ip(request: Request, call_next):
+    """Простая защита от большого количества запросов за короткий промежуток времени по IP"""
     ip = str(request.client.host)
     now = dt.utcnow()
 
-    if ip in strange_ips:
-
-        visits = strange_ips[ip]["visits"]
-
-        if visits == 15:
-            banned_ips[ip] = now
-            strange_ips.pop(ip, None)
-
-        elif now - strange_ips[ip]["first_request_dt"] < timedelta(seconds=15):
-            visits += 1
-            strange_ips[ip]["visits"] = visits
-
-    elif ip not in banned_ips:
-        strange_ips[ip] = {"visits": 1, "first_request_dt": now}
-
     if ip in banned_ips:
 
-        if now - banned_ips[ip] > timedelta(seconds=15):
+        if now - banned_ips[ip] > timedelta(seconds=10):
             banned_ips.pop(ip, None)
+            strange_ips.pop(ip, None)
             return await call_next(request)
 
         data = {"message": f"IP {ip} was temporarily banned."}
-
         return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=data)
 
+    if ip in strange_ips:
+        visits = strange_ips[ip]["visits"]
+        if visits >= 15:
+            banned_ips[ip] = now
+        if now - strange_ips[ip]["first_request_dt"] < timedelta(seconds=10):
+            visits += 1
+        else:
+            strange_ips[ip]["first_request_dt"] = now
+            visits = 1
+        strange_ips[ip]["visits"] = visits
+    else:
+        strange_ips[ip] = {"visits": 1, "first_request_dt": now}
     return await call_next(request)
 
 
 @app.get("/")
 async def root(request: Request):
+    """Страница на которой засчитываются посещения"""
     headers = request.headers
     platform = "".join(s for s in str(headers.get("sec-ch-ua-platform")) if s.isalpha() or s.isspace())
     agent = str(headers.get("user-agent"))
@@ -101,9 +102,10 @@ async def root(request: Request):
 
 
 @app.get("/api/v1/get/")
-async def v1_get_visitors(filters: Filter = Depends()):
+async def v1_get_visitors(request: Request, filters: Filter = Depends()):
+    """Страница с API v1"""
     if new_visitors:
         save_new_visitors(DB_CONNECTION, new_visitors)
         new_visitors.clear()
-    data = {"message": None}
+    data = {"message": filters.model_dump(mode="json")}
     return JSONResponse(status_code=status.HTTP_200_OK, content=data)
--- source/classes.py
+++ source/classes.py
@@ -1,5 +1,5 @@
 from datetime import datetime as dt, datetime
-from typing import Any, Optional
+from typing import Optional
 
 from fastapi import Query
 from pydantic import (BaseModel, Field, conint,
@@ -13,34 +13,34 @@ class Visitor(BaseModel):
     agent: str = "NULL"  # optional
 
 
+# noinspection PyNestedDecorators
 class Filter(BaseModel):
     start: Optional[str] = Field(Query(None, description="Represents the start date & time"))
     end: Optional[str] = Field(Query(None, description="Represents the end date & time"))
     like: Optional[str] = Field(Query(None, description="Represents a keyword or pattern to filter results"
                                                         " based on a partial match. Example: \"apple\" to "
                                                         "filter results containing the word \"apple\"."))
-    limit: conint(ge=0, le=100) = 10
-    utc: float = Field(default=0, ge=-12, le=12)
+    limit: conint(ge=1, le=100) = 10
+    utc: float = Field(Query(default=0, ge=-12, le=12))
     unique: bool = False
 
-    @classmethod
     @field_validator("start", "end")
+    @classmethod
     def validate_str_datetime(cls, value):
         """Проверка даты, написанной строкой слитно в
             формате ДД-ММ-ГГГГ:чч:мм:cc (exp: 16-05-2022:01:50:30)"""
         if value is None:
-            return value
+            return None
         try:
-            datetime.strptime(value, "%d-%m-%Y:%H:%M:%S")
+            return datetime.strptime(value, "%d-%m-%Y:%H:%M:%S")
         except (ValueError, TypeError):
             try:
-                datetime.strptime(value, "%d-%m-%Y")
+                return datetime.strptime(value, "%d-%m-%Y")
             except (ValueError, TypeError):
                 raise ValueError("Input should be DD-MM-YYYY:hh:mm:ss or DD-MM-YYYY")
-        return value
 
-    @classmethod
     @field_validator("like")
+    @classmethod
     def validate_isalnum(cls, value):
         if value is None:
             return value
@@ -48,10 +48,21 @@ class Filter(BaseModel):
             return value
         raise ValueError("Input should contain letters or numbers")
 
-    @classmethod
     @field_validator("utc")
+    @classmethod
     def utc_validator(cls, utc):
         """Проверка валидности UTC"""
         if utc in map(lambda x: x / 10, range(-120, 121, 5)):
             return utc
         raise ValueError("Input should be an integer or half number")
+
+    @model_validator(mode="after")
+    def check_start_earlier_than_end(self) -> "Filter":
+        start, end = self.start, self.end
+        if start is not None and end is not None:
+            if start > end:
+                raise ValueError("'start' datetime should be less or equal than 'end' datetime")
+
+        self.start = str(start) if start is not None else None
+        self.end = str(end) if start is not None else None
+        return self
